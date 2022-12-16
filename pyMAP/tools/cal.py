a66693aafd5dd572fafe4c288f84e7a50fba2a9e
jonbowr <jonbowr@yahoo.com>
2022-12-16 16:44:13 -0500

    updated asplanned table, thinking about adding test repo builder to cal package

--- pyMAP/tools/cal.py
+++ pyMAP/tools/cal.py
@@ -2,23 +2,6 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-
-def calc_checksum(df_gt):
-    return((df_gt['TOF0']+df_gt['TOF3']-df_gt['TOF2']-df_gt['TOF1']))
-
-def checksum(df_gt,check_max = 1):
-    return(np.logical_and.reduce([abs(df_gt['TOF3'])<15,
-            calc_checksum(df_gt)<check_max]))
-
-def filt_trips(athing):
-    log_good = []
-    for stuff in athing:
-        if 'validtof' in stuff.lower():
-
-            # print(stuff)
-            log_good.append(athing[stuff].values.astype(bool))
-    return(np.logical_and.reduce(log_good))
-
 def cal_headder(fil):
     stuff = ''
     for t in open(fil).readlines():
@@ -38,9 +21,3 @@ def cal_headder(fil):
                 print('dat import failed: %s'%fil)
                 return
     return(head)
-
-def load(fil):
-    head = cal_headder(fil)
-    athing = pd.read_csv(fil,comment = '#',delim_whitespace= True,header = None,names = head)
-    return(athing)
-
