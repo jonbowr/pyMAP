cc77216300afa8f557fda148d782152ca51f3cee
jonbowr <jonbowr@yahoo.com>
2023-04-19 16:01:37 -0400

    removed plot groups from jill data groups file

--- pyMAP/jill/data_groups.py
+++ pyMAP/jill/data_groups.py
@@ -10,17 +10,3 @@ data_groups = {
             }
         }
 
-# define dict with same keys as data_groups to setup plot groups of data
-#   might want to move this to a different locaiton or combine somehow with data_groups
-plt_groups = {
-        'ILO_EM_status':
-            {
-            'single_rates [cts/s]':['START_A', 'START_C', 'STOP_B0', 'STOP_B3'],                 
-            'tof_rate[cts/s]':['TOF0','TOF1','TOF2','TOF3','SILVER'],             
-            'Efficiency':['Eff_A','Eff_B','Eff_C','Eff_TRIP'],      
-           'Monitors':['MCP_VM','MCP_CM','MCP_VSET','PAC_VSET','PAC_VM','PAC_CM'],          
-           'volts[V]':['TOF_MCP_VM','PAC_VM_volt'],  
-           'Board Temp':['TEMP1','TEMP0','LV_TEMP','MCP_TEMP','PAC_TEMP'],        
-           'Threshold register':['AN_A_THR_REG','AN_B0_THR_REG','AN_B3_THR_REG','AN_C_THR_REG'],                    
-           } 
-}
