fd066f98bd45beee095564f961823f21b67c2ac5
jonbowr <jonbowr@yahoo.com>
2023-06-21 15:52:30 -0400

    added function to determine the angle setting from the time to use when stage is scanning

--- pyMAP/data/instrument/IMAP_lo_EMStrSen/load.py
+++ pyMAP/data/instrument/IMAP_lo_EMStrSen/load.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
 
+def fix_angle(df):
+    # loc = abs(df['azm'])==999
+    time = df.reset_index()['time']
+    df['scan_dir'] = df['azm']/abs(df['azm'])
+    ang = (time-np.mean(time))*df['azm']/abs(df['azm'])
+    df['phi'] = ang
+    return(df)
+
 def load_strSen_v1(loc):
     head = ['time','v_pmt','azm','v_str']
     df = pd.read_csv(loc,header = None,delimiter='\t',names = head)
-    return(df)
-
+    return(fix_angle(df))
 
 loadlib = {
             'strSen':{
@@ -18,3 +25,4 @@ def load(as_runloc,dtype = 'strSen',version = 'v001'):
     df = loadlib[dtype][version](as_runloc)
 
     return(df)
+
