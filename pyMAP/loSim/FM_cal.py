36b376b2a6a336196a0def7d2792fc2933b4abd0
jonbowr <jonbowr@yahoo.com>
2025-03-20 14:01:33 -0400

    update H model scattering params to fit results from 20241220

--- pyMAP/loSim/FM_cal.py
+++ pyMAP/loSim/FM_cal.py
@@ -12,36 +12,59 @@ def sim_h2_dispersion(n,mean = 100,fwhm = 50):
     return(e_diss(E0,Ed = 4.52,pm = pm,alpha = alpha))
 
 
-def set_simulator_cal(sim, species = 'H',result = 'fin_cal1'):
+def set_simulator_cal(sim, species = 'H',result = 'fin_cal_H_2'):
     '''
     Function to setup instrument scattering simulator to calibrated values from FinalCal
     '''
 
 
-    def fin_cal1_H(sim,species):
+    def fin_cal_H_1(sim):
         '''
         Simulator settings defined following IMAP-Lo FM cal, 12/16/2024
         - model regression performed primarily against final cal results, 20241212-FMv3_T105_PSPL_FinalCal_DER.csv
         - model checked against IBEX-lo H final cal results and precal2 results
         - see 20241204_FinalCal_DER1 H Model Comparison.ipynb for exact definition
         '''
+          
+        sim[1].part['surf_binding'] = .25
+        sim[1].part['sputtering'] = .2
+        sim.params['cs_scatter']['ke']['pdf']['b'] = .3
+        sim[1].effic.p0 =sim[1].cal_fits['effic']['He'].p0*.0968*.68
 
-            
-        if species == 'H':
-            sim[1].part['surf_binding'] = .25
-            sim[1].part['sputtering'] = .2
-            sim.params['cs_scatter']['ke']['pdf']['b'] = .3
-            sim[1].effic.p0 =sim[1].cal_fits['effic']['He'].p0*.0968*.68
+        sim[1].scatter_type = 'inelastic'
+        sim[1].ke['modulator_f'] = make_f_eloss()[1]
+        return(sim)
 
-            sim[1].scatter_type = 'inelastic'
-            sim[1].ke['modulator_f'] = make_f_eloss()[1]
+
+    def fin_cal_H_2(sim):
+        '''
+        Simulator settings defined following IMAP-Lo FM cal
+        - model regression performed primarily against final cal results, 20241219-FMv3_T105_PSPL_FinalCal_DER.csv
+            - also possibly agains 20241220
+        - model checked against IBEX-lo H final cal results and precal2 results
+        - see 20241204_FinalCal_DER1 H Model Comparison.ipynb for exact definition
+        '''
+        from numpy import array
+        sim[1].part['surf_binding'] = .1
+        sim[1].part['sputtering'] = .2
+        sim.params['cs_scatter']['ke']['pdf']['b'] = .3
+        sim[1].effic.p0 =array([0.00721197/.8, 0.19172372*.8])*.9
+
+        sim[1].scatter_type = 'inelastic'
+        sim[1].ke['modulator_f'] = make_f_eloss()[1]
+        
+        sim[1].theta['modulator_f'].p0 = array([0.92923537, 2.        , 0.1530386 ])
+        sim[1].phi['modulator_f'].p0 = array([ 1.16825037, 36.99865872,  0.22471439])
         return(sim)
 
     dict_cals = {
-                    'fin_cal1': fin_cal1_H,
+                    'H':{
+                        'fin_cal1': fin_cal_H_1,
+                        'fin_cal2': fin_cal_H_2
+                        }
                 }
 
-    return(dict_cals[result](sim,species))
+    return(dict_cals[species][result](sim))
 
 
 def make_f_eloss():
