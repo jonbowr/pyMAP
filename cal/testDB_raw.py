724e91be1f2b43acff9ff28986f0b7378215f58d
jonbowr <jonbowr@yahoo.com>
2026-01-09 13:38:25 -0500

    added comprehensive testDB index

---
+++ cal/testDB_raw.py
@@ -0,0 +1,139 @@
+
+
+test_db = {
+            'T001':{
+                    'testDB':r'C:\Users\Jonny Woof\Box\IMAP-Lo-box (1)\Science\Testing\IMAP_lo\EMv1\EMv1_T001-ToF First Light Princeton',
+                    'asrun':r'EMv1_ToF_FirstLight_PSPL_AsRun.xlsx',
+                    'testTitle':'EMv1_T001-ToF_First_Light_Princeton',
+                    'asrun_pages':['Global','ETU_tof','ETU_sensor','Princeton_SPL'],
+                    'snifferDB':'',
+                    'eboxDB':r'\Test Data\csv',
+                    'instrument': 'EMv1'
+                    },
+            'T002': {
+                    'testDB':r'C:\Users\Jonny Woof\Box\IMAP-Lo-box (1)\Science\Testing\IMAP_lo\EMv1\EMv1_T002-ToF First Light UNHSPLAT',
+                    'asrun':r'EMv1_UNHSPLAT_FirstLight-AsRun.xlsx',
+                    'testTitle':'EMv1_T002-ToF_First_Light_UNHSPLAT',
+                    'asrun_pages':['Global','M145_beam','M145_system','ETU_tof'],
+                    'snifferDB':'',
+                    'eboxDB':r'\Test Data\csv',
+                    'instrument': 'EMv1'
+                    },
+            'T003': {
+                    'testDB':r'C:\Users\Jonny Woof\Box\IMAP-Lo-box (1)\Science\Testing\IMAP_lo\EMv1\EMv1_T003-ToF Cal Beam Test UNHSPLAT',
+                    'asrun':'EMv1_ToFcal_Beam_Test_AsRun.xlsx',
+                    'testTitle':'EMv1_T003-ToF_Cal_Beam_Test_UNHSPLAT',
+                    'asrun_pages':['Global','M145_beam','M145_system','ETU_tof'],
+                    'snifferDB':'',
+                    'eboxDB':r'\Test Data\csv',
+                    'instrument': 'EMv1'
+                    },
+            'T004': {
+                    'testDB':r'C:\Users\Jonny Woof\Box\IMAP-Lo-box (1)\Science\Testing\IMAP_lo\EMv1\EMv1_T004-ToF Cal Beam Test 2 UNHSPLAT',
+                    'asrun':'EMv1_ToF_Cal_Beam_Test2_AsRun.xlsx',
+                    'testTitle':'EMv1_ToF_Cal_Beam_Test2_UNHSPLAT',
+                    'asrun_pages':['Global','M145_beam','M145_system','EM_tof'],
+                    'snifferDB':r'\Test Data\Sniffer',
+                    'eboxDB':r'\Test Data\Sensor\csv',
+                    'instrument': 'EMv1'
+                    },
+            'T005': {
+                    'testDB':r'C:\Users\Jonny Woof\Box\IMAP-Lo-box (1)\Science\Testing\IMAP_lo\EMv1\EMv1_T005_PSPL_ToFCal3',
+                    'asrun':'EMV1_T005_PSPL_ToFCal3_AsRun.xlsx',
+                    'testTitle':'EMV1_T005_PSPL_ToFCal3',
+                    'asrun_pages':['Global','EM_tof','Princeton_PSPL'],
+                    'snifferDB':r'\Test Data\Sniffer',
+                    'eboxDB':r'\Test Data\Sensor\csv',
+                    'instrument': 'EMv1'
+                    },
+            'T006':{
+                    'testDB':r'C:\Users\Jonny Woof\Box\IMAP-Lo-box (1)\Science\Testing\IMAP_lo\EMv2\EMV2_T006_PSPL_FirstLight',
+                    'asrun':'EMv2_T006_PSPL_FirstLight_AsRun.xlsx',
+                    'testTitle':'EMv2_T006_PSPL_FirstLight',
+                    'asrun_pages':['Global','EM_tof','Princeton_PSPL'],
+                    'snifferDB':r'\Test Data\Sniffer',
+                    'eboxDB':r'\Test Data\Sensor',
+                    'instrument': 'EMv2'
+                    },
+            'T007':{
+                    'testDB':r'C:\Users\Jonny Woof\Box\IMAP-Lo-box (1)\Science\Testing\IMAP_lo\EMv2\EMV2_rev2_T007_PSPL_FirstLight',
+                    'asrun':'',
+                    'testTitle':'EMv2_rev2_T007_PSPL_FirstLight',
+                    'asrun_pages':[],
+                    'snifferDB':r'\Test Data\Sniffer',
+                    'eboxDB':r'\Test Data\Sensor',
+                    'instrument': 'EMv2'
+                    },
+            'T011':{
+                    'testDB':r'C:\Users\Jonny Woof\Box\IMAP-Lo-box (1)\Science\Testing\IMAP_lo\EMv2\EMv2c_T011_PSPL_FirstLight_20230424',
+                    'asrun':'EMv2c_T011_PSPL_FirstLight_20230424_AsRun.xlsx',
+                    'testTitle':'EMv2c_T011_PSPL_FirstLight_20230424',
+                    'asrun_pages':['Global','EM_optics','EM_tof','Princeton_PSPL'],
+                    'snifferDB':r'\Test Data\Sensor\csv',
+                    'eboxDB':r'',
+                    'instrument': 'EMv2c'
+                    },
+            'T012':{
+                    'testDB':r'C:\Users\Jonny Woof\Box\IMAP-Lo-box (1)\Science\Testing\IMAP_lo\EMv2\EMv2c_T012_PSPL_20230516',
+                    'asrun':'EMv2c_T012_PSPL_20230515_AsRun.xlsx',
+                    'testTitle':'EMv2c_T012_PSPL_20230516',
+                    'asrun_pages':['Global','EM_optics','EM_tof','Princeton_PSPL'],
+                    'snifferDB':r'\Test Data\Sensor\csv',
+                    'eboxDB':r'',
+                    'instrument': 'EMv2c'
+                    },
+            'T101':{
+                    'testDB':r'C:\Users\Jonny Woof\OneDrive - USNH\IMAP-Lo_Cal_Science\IMAP-Lo_Cal_DB\IMAP-Lo_FM_CAL\FMv1a_T101_PSPL_ToF_Cal',
+                    'asrun':'FMv1a_T101_PSPL_ToF_Cal_AsRun.xlsx',
+                    'testTitle':'FMv1a_T101_PSPL_ToF_Cal',
+                    'asrun_pages':['Global','FM_tof','Princeton_PSPL'],
+                    'snifferDB':r'\Test Data\Sniffer',
+                    'eboxDB':r'\Test Data\Sensor',
+                    'instrument': 'FMv1a'
+                    },
+            'T102':{
+                    'testDB':r'C:\Users\Jonny Woof\OneDrive - USNH\IMAP-Lo_Cal_Science\IMAP-Lo_Cal_DB\IMAP-Lo_FM_CAL\FMv2_T102_PSPL_PreCal1',
+                    'asrun':'FMv2_T102_PSPL_PreCal1_AsRun.xlsx',
+                    'testTitle':'FMv2_T102_PSPL_PreCal1',
+                    'asrun_pages':['Global','FM_tof','FM_optics','Princeton_PSPL'],
+                    'snifferDB':r'\Test Data\Sniffer',
+                    'eboxDB':r'\Test Data\Sensor',
+                    'instrument': 'FMv2'
+                    },
+            'T103':{
+                    'testDB':r'C:\Users\Jonny Woof\OneDrive - USNH\IMAP-Lo_Cal_Science\IMAP-Lo_Cal_DB\IMAP-Lo_FM_CAL\FMv3_T103_PSPL_PreCal2',
+                    'asrun':'FMv3_T103_PSPL_PreCal2_AsRun.xlsx',
+                    'testTitle':'FMv3_T103_PSPL_PreCal2',
+                    'asrun_pages':['Global','TOF','FM_optics','Princeton_PSPL'],
+                    'snifferDB':r'\Test Data\Sniffer',
+                    'eboxDB':r'\Test Data\Sensor',
+                    'instrument': 'FMv3'
+                    },
+            'T104':{
+                    'testDB':r'C:\Users\Jonny Woof\OneDrive - USNH\IMAP-Lo_Cal_Science\IMAP-Lo_Cal_DB\IMAP-Lo_FM_CAL\FMv3_T104_PSPL_PreCal2',
+                    'asrun':'FMv3_T104_PSPL_PreCal2_AsRun.xlsx',
+                    'testTitle':'FMv3_T104_PSPL_PreCal2',
+                    'asrun_pages':['Global','TOF','FM_optics','Princeton_PSPL'],
+                    'snifferDB':'',
+                    'eboxDB':r'\Test Data\Sensor',
+                    'instrument': 'FMv3'
+                    },
+            'T105':{
+                    'testDB':r'C:\Users\Jonny Woof\OneDrive - USNH\IMAP-Lo_Cal_Science\IMAP-Lo_Cal_DB\IMAP-Lo_FM_CAL\FMv3_T105_PSPL_FinalCal',
+                    'asrun':'FMv3_T105_PSPL_FinalCal_AsRun.xlsx',
+                    'testTitle':'FMv3_T105_PSPL_FinalCal',
+                    'asrun_pages':['Global','TOF','FM_optics','Princeton_PSPL'],
+                    'snifferDB':'',
+                    'eboxDB':r'\Test Data\Instrument Data',
+                    'instrument': 'FMv3'
+                    },
+            'T106':{
+                    'testDB':r'C:\Users\Jonny Woof\OneDrive - USNH\IMAP-Lo_Cal_Science\IMAP-Lo_Cal_DB\IMAP-Lo_FM_CAL\FMv3_T106_LANL_CrossCal',
+                    'asrun':'FMv3_T106_LANL_CrossCal_AsRun.xlsx',
+                    'testTitle':'FMv3_T106_LANL_CrossCal',
+                    'asrun_pages':['Global','TOF','FM_optics','LANL'],
+                    'snifferDB':'',
+                    'eboxDB':r'\Test Data\Instrument Data',
+                    'instrument': 'FMv3'
+                    },
+            }
